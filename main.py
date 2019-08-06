3dab7fe44678f8814e0f39b6d532bcaecff796fa
Camilo Santos <camilo@camilo-debian>
2019-08-06 13:33:00 -0300

    add vdofinalizadora

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import asyncpg
-from models import VdoFaixaHora
+from typing import List
+from models import VdoFaixaHora, VdoFinalizadora
 
 
 async def get_unidades(conn):
@@ -10,16 +11,40 @@ async def get_unidades(conn):
     return [unid["uni_codigo"] for unid in values]
 
 
+async def get_finalizadoras(conn) -> List[str]:
+    stmt = await conn.prepare('''
+        select fin_codigo
+        from finalizadoras
+        group by fin_codigo;''')
+    values = await stmt.fetch()
+    return [fin["fin_codigo"] for fin in values]
+
+
+async def get_pvds(conn) -> List[str]:
+    stmt = await conn.prepare('''
+        select est_codigo
+        from estac
+        group by est_codigo;''')
+    values = await stmt.fetch()
+    return [pdv["est_codigo"] for pdv in values]
+
+
 async def main():
     conn = await asyncpg.connect(
         user='postgres', password='rp1064',
         database='wrpdv_df', host='127.0.0.1'
     )
     unids = await get_unidades(conn)
+    fin = await get_finalizadoras(conn)
+    pdvs = await get_pvds(conn)
 
     faixa_hora = VdoFaixaHora(conn, unids)
+    finalizadora = VdoFinalizadora(conn, unids, pdvs, fin)
 
-    await asyncio.gather(faixa_hora.run())
+    await faixa_hora.run()
+    await finalizadora.run()
+
     await conn.close()
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
--- models.py
+++ models.py
@@ -1,12 +1,40 @@
+import abc
 import random
+from datetime import datetime
 from typing import List
+
 
 from utils import generate_decimal
 
 Unids = List[str]
+Pdvs = List[str]
+Final = List[str]
 
 
-class VdoFaixaHora:
+class Vdo(abc.ABC):
+    @abc.abstractmethod
+    async def _exists(self) -> bool:
+        pass
+
+    @abc.abstractmethod
+    async def _update(self):
+        pass
+
+    @abc.abstractmethod
+    async def _insert(self):
+        pass
+
+    async def run(self):
+        exist = await self._exists()
+        if exist:
+            print("update", self.__class__.__name__, datetime.now())
+            await self._update()
+        else:
+            print("insert", self.__class__.__name__, datetime.now())
+            await self._insert()
+
+
+class VdoFaixaHora(Vdo):
     def __init__(self, conn, unids: Unids):
         self.conn = conn
         self.unids: Unids = unids
@@ -49,11 +77,58 @@ class VdoFaixaHora:
             generate_decimal(), generate_decimal(), generate_decimal(),
             random.choice(self.unids))
 
-    async def run(self):
-        exist = await self._exists()
-        if exist:
-            print("update")
-            await self._update()
-        else:
-            print("insert")
-            await self._insert()
+
+class VdoFinalizadora(Vdo):
+    def __init__(self, conn, unids: Unids, pdvs: Pdvs, fin: Final):
+        self.conn = conn
+        self.unids: Unids = unids
+        self.pdvs: Pdvs = pdvs
+        self.fin: Final = fin
+
+    async def _exists(self) -> bool:
+        stmt = await self.conn.prepare('''select exists(
+            select * from vdofinalizadoras
+            where vfi_data=current_date
+        )''')
+        value = await stmt.fetchrow()
+        return value['exists']
+
+    async def _insert(self):
+        await self.conn.execute('''
+            INSERT INTO public.vdofinalizadoras (vfi_codseq,
+            vfi_unidade,vfi_pdv,vfi_data,vfi_finalizadora,
+            vfi_descricao,vfi_vendabruta,vfi_cancelamentos,
+            vfi_acrescimos,vfi_descontos,vfi_qtditens,vfi_qtdcupons)
+            with fin as (
+                select fin_codigo,initcap(fin_descricao) as fin_descricao
+                from finalizadoras
+                group by fin_codigo,initcap(fin_descricao)
+            )
+            select
+                nextval('generate_vdo'),uni_codigo,est_codigo,current_date,
+                fin_codigo,fin_descricao,$1,$2,$3,$4,$5,$6
+            from unidades
+                inner join estac on (uni_codigo=est_unidade),
+                fin
+            where uni_ativa='S';''', generate_decimal(100), generate_decimal(),
+                                generate_decimal(), generate_decimal(),
+                                generate_decimal(), generate_decimal())
+
+    async def _update(self):
+        unid = random.choice(self.unids)
+        pdv = random.choice(self.pdvs)
+        fin = random.choice(self.fin)
+        await self.conn.execute('''UPDATE vdofinalizadoras
+        SET vfi_vendabruta=vfi_vendabruta+$1,
+            vfi_cancelamentos=vfi_cancelamentos+$2,
+            vfi_acrescimos=vfi_acrescimos+$3,
+            vfi_descontos=vfi_descontos+$4,
+            vfi_qtditens=vfi_qtditens+$5,
+            vfi_qtdcupons=vfi_qtdcupons+$6
+        where vfi_data=current_date
+            and vfi_unidade=$7
+            and vfi_pdv=$8
+            and vfi_finalizadora=$9;
+        ''', generate_decimal(100), generate_decimal(), generate_decimal(),
+            generate_decimal(), generate_decimal(), generate_decimal(),
+            unid, pdv, str(fin))
